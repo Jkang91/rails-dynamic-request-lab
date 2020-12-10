85f72076585105676b54ae43ea62803f52ba90f4
June Kang <Jkang34@gmail.com>
2020-12-09 19:38:17 -0500

    damn this lab

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,16 @@
 class StudentsController < ApplicationController
+  before_action :student_name, only: :show
+  
   def index
     @students = Student.all
+  end
+
+  def show
+  end
+
+  private
+
+  def student_name
+    @student = Student.find(params[:id])
   end
 end
